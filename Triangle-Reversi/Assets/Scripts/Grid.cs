8f58961afae3b8be8ee74ca9d820c7826a85a88d
Shachar <nihohit@gmail.com>
2021-01-03 11:30:06 +0200

    Add player coloring

--- Triangle-Reversi/Assets/Scripts/Grid.cs
+++ Triangle-Reversi/Assets/Scripts/Grid.cs
@@ -8,15 +8,25 @@ public class Grid : MonoBehaviour {
 
   public GridShape shape;
   public int length;
+  private readonly Dictionary<Vector2Int, Triangle> map = new Dictionary<Vector2Int, Triangle>();
 
   void initializeTriangle(GameObject trianglePrefab) {
     for (int i = 0; i < length; ++i) {
       for (int j = -i; j <= i; ++j) {
-        Debug.Log($"iterate over {i} {j}");
         var triangle = Instantiate(trianglePrefab).GetComponent<Triangle>();
         triangle.Setup(j, i - (length / 2), Math.Abs(j + i) % 2 == 1, this);
+        map.Add(new Vector2Int(j, i - (length / 2)), triangle);
+        Debug.Log(new Vector2Int(j, i - (length / 2)));
       }
     }
+    map[new Vector2Int(1, 0)].SetOwner(Triangle.Owner.Blue);
+    map[new Vector2Int(-1, 0)].SetOwner(Triangle.Owner.Red);
+    map[new Vector2Int(-1, 1)].SetOwner(Triangle.Owner.Blue);
+    map[new Vector2Int(1, 1)].SetOwner(Triangle.Owner.Red);
+    map[new Vector2Int(1, -1)].SetOwner(Triangle.Owner.Red);
+    map[new Vector2Int(-1, -1)].SetOwner(Triangle.Owner.Blue);
+    map[new Vector2Int(-1, 2)].SetOwner(Triangle.Owner.Red);
+    map[new Vector2Int(1, 2)].SetOwner(Triangle.Owner.Blue);
   }
 
   // Start is called before the first frame update
--- Triangle-Reversi/Assets/Scripts/Triangle.cs
+++ Triangle-Reversi/Assets/Scripts/Triangle.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Triangle : MonoBehaviour {
+  public enum DisplayColor { White, Yellow, Red, Blue };
+  public enum Owner { None, Red, Blue };
+
   public int xIndex;
   public int yIndex;
   public bool inverted;
+  public DisplayColor displayColor;
+  public Owner owner;
 
   private const float kSpacing = 0.05F;
   private const float kScale = 1F / 3F;
   private Grid grid;
+  private SpriteRenderer spriteRenderer;
 
   // Start is called before the first frame update
-  void Start() {
+  void Awake() {
     this.transform.localScale = new Vector3(kScale, kScale, 1);
+    spriteRenderer = GetComponent<SpriteRenderer>();
   }
 
   void OnMouseDown() {
@@ -21,7 +28,6 @@ public class Triangle : MonoBehaviour {
   }
 
   public void Setup(int x, int y, bool inverted, Grid grid) {
-    Debug.Log($"setup {x} {y}");
     this.xIndex = x;
     yIndex = y;
     this.inverted = inverted;
@@ -29,8 +35,50 @@ public class Triangle : MonoBehaviour {
     if (inverted) {
       this.transform.eulerAngles = new Vector3(0, 0, 180);
     }
-    this.name = $"Triangle-{x}-{y}";
+    this.name = $"Triangle {x} {y}";
 
     this.transform.position = new Vector3(xIndex * (0.5F + kSpacing), yIndex * (-1 - kSpacing), 0);
+  }
+
+  private Color colorForDisplayColor(DisplayColor color) {
+    switch (color) {
+      case DisplayColor.Blue:
+        return Color.blue;
+      case DisplayColor.Red:
+        return Color.red;
+      case DisplayColor.White:
+        return Color.white;
+      case DisplayColor.Yellow:
+        return Color.yellow;
+      default:
+        return Color.white;
+    }
+  }
+
+  private DisplayColor displayColorForOwner(Owner owner) {
+    switch (owner) {
+      case Owner.None:
+        return DisplayColor.White;
+      case Owner.Blue:
+        return DisplayColor.Blue;
+      case Owner.Red:
+        return DisplayColor.Red;
+      default:
+        return DisplayColor.White;
+    }
+  }
+
+  public void SetDisplayColor(DisplayColor color) {
+    displayColor = color;
+    spriteRenderer.color = colorForDisplayColor(color);
+  }
+
+  public void ResetDisplayColor() {
+    SetDisplayColor(displayColorForOwner(owner));
+  }
+
+  public void SetOwner(Owner owner) {
+    this.owner = owner;
+    ResetDisplayColor();
   }
 }
